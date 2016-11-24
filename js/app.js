98b17a40228d6b6a25d0a2a9f36044dae5d1a461
Tobias Gerhardsson <tobias.gerhardsson@gmail.com>
2016-11-24 11:53:50 +0100

    Added function for returning local IP adress dynamically
    Thanks to furbel and Alling over at Sweclockers for the help!

--- js/app.js
+++ js/app.js
@@ -1,18 +1,43 @@
 // REMINDER:
 // Add array/object for replacements?
 
+function getLocalIPAddress(success) {
+    window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
+    var pc = new RTCPeerConnection({
+            iceServers: []
+        }),
+        noop = function() {};
+    pc.createDataChannel(""); //create a bogus data channel
+    pc.createOffer(pc.setLocalDescription.bind(pc), noop); // create offer and set local description
+
+    pc.onicecandidate = function(ice) { //listen for candidate events
+        if (!ice || !ice.candidate || !ice.candidate.candidate) return;
+        myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+        pc.onicecandidate = noop;
+
+        success(myIP);
+    };
+}
+
 //Clicking button
 $("button").click(function() {
-    $("input").removeClass("enter");
-    $("input").addClass("enter");
+
     var $textArea = $("input");
+
     //Entered texts value
     var oldText = $textArea.val();
-    //Entered texts value, with words replaced
-    var newText = oldText.replace("localhost", "192.168.0.12");
-    //Replace old value with new value and select it
-    $textArea.val(newText).select();
-    //Copy new text to clipboard and view new text in textarea
-    document.execCommand('copy');
-    $textArea.val(newText);
+
+    getLocalIPAddress(function(ip) {
+
+        //Entered texts value, with words replaced
+        var newText = oldText.replace("localhost", ip);
+
+        console.log(newText);
+        //Replace old value with new value and select it
+        $textArea.val(newText).select();
+        //Copy new text to clipboard and view new text in textarea
+        document.execCommand('copy');
+        $textArea.val(newText);
+
+    });
 });
